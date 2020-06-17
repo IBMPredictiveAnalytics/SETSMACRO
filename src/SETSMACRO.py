9f69fc4ecc49e8d7f6fd1ae921bf9ee35bebf6cd
Juan Wu <juanwuxa@cn.ibm.com>
2020-06-16 20:25:29 -0700

    upgrade extension

--- src/SETSMACRO.py
+++ src/SETSMACRO.py
@@ -3,7 +3,7 @@
 # *
 # * IBM SPSS Products: Statistics Common
 # *
-# * (C) Copyright IBM Corp. 1989, 2014
+# * (C) Copyright IBM Corp. 1989, 2020
 # *
 # * US Government Users Restricted Rights - Use, duplication or disclosure
 # * restricted by GSA ADP Schedule Contract with IBM Corp. 
@@ -112,7 +112,7 @@ holds just the names.
 def Run(args):
         """Execute the SETSMACRO command"""
 
-        args = args[args.keys()[0]]
+        args = args[list(args.keys())[0]]
         ###print args   #debug
         oobj = Syntax([
                 Template("SETS", subc="",  ktype="literal", var="setnames", islist=True),
@@ -123,7 +123,7 @@ def Run(args):
                 Template("HELP", subc="", ktype="bool")])
 
         # A HELP subcommand overrides all else
-        if args.has_key("HELP"):
+        if "HELP" in args:
             #print helptext
             helper()
         else:
@@ -143,7 +143,7 @@ def helper():
     # webbrowser.open seems not to work well
     browser = webbrowser.get()
     if not browser.open_new(helpspec):
-        print("Help file not found:" + helpspec)
+        print(("Help file not found:" + helpspec))
 try:    #override
     from extension import helper
 except:
